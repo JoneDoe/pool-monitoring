a9133b58c551c4ae916e90292802501cf8c31d9b
Vadim Titov <vadim.titov@goconsensus.com>
2024-01-17 20:45:38 +0200

    fix vertical orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'pages/main_dashboard.dart';
 
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: CryptoDashboard(),
