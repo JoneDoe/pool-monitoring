30c030a6d5702ad3efbf58f5c310f0a5fd7ecd7d
Vadim Titov <vadim.titov@goconsensus.com>
2024-01-23 22:04:59 +0200

    fix chart

--- lib/chart/providers/herominers_daily_stat.dart
+++ lib/chart/providers/herominers_daily_stat.dart
@@ -6,6 +6,13 @@ import '../bar_chart_data.dart';
 class HerominersDailyStat extends AbstractDailyStat {
   @override
   AppBarChartData load(dynamic map, Config config) {
+    var index = 7 - map['unlocked_daily'].length;
+
+    while (index > 0) {
+      myBarData.addValue(0);
+      index--;
+    }
+
     map['unlocked_daily'].forEach((data) {
       myBarData.addValue(double.parse(data) / config.coinUnits);
     });
